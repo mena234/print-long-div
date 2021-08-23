e5a9e3f8bd3db3cb8ccac3055c63a22e202b0cc5
Mena Nasser <menanasser234@outlook.sa>
2021-08-23 21:42:33 +0200

    Init Project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +7,29 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'print-long-div';
+  @ViewChild('printableDiv') printableDiv: ElementRef<HTMLDivElement>;
+
+  print() {
+    const mywindow = window.open('', 'PRINT', 'height=400,width=600');
+    mywindow.document.write('<html><head><title>' + document.title  + '</title>');
+    mywindow.document.write('<link rel="stylesheet" href="style.css"></link>');
+    mywindow.document.write(`<style>
+@page {
+    size: 33mm 45mm;
+    margin: 5%;
+}
+    </style>`)
+    mywindow.document.write('</head><body >');
+    mywindow.document.write('<h1>' + document.title  + '</h1>');
+    mywindow.document.write(this.printableDiv.nativeElement.innerHTML);
+    mywindow.document.write('</body></html>');
+
+    mywindow.document.close(); // necessary for IE >= 10
+    mywindow.focus(); // necessary for IE >= 10*/
+
+    mywindow.print();
+    // mywindow.close();
+
+    // return true;
+  }
 }
